6b3dd4a50813cd330eb4dd0c7ce05af161a806c3
difre87 <adjeifrederic@gmail>
2026-04-23 09:21:14 +0000

    feat: add favicon and update HTML head for improved branding

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,11 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+        <link rel="alternate icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" href="/favicon.svg">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
